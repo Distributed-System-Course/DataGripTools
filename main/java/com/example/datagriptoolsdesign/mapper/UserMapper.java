f052a1183e08e9312a50a397d37d4c073b449082
mubai <ArgoMuBai@outlook.com>
2021-11-09 21:35:18 +0800

    加入了MyBatis对数据库的操作，通过控制器来完成对Data_Process对象的调用并将数据注入到数据库中

--- main/java/com/example/datagriptoolsdesign/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.datagriptoolsdesign.mapper;
-
-import com.example.datagriptoolsdesign.bean.UserBean;
-import org.apache.ibatis.annotations.Mapper;
-
-
-public interface UserMapper {
-    UserBean getInfo(String name, String password);
-}
