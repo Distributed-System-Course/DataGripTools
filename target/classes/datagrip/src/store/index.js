61fdbbb38acf7fa90103206f35536e5bd14fb981
mubai <ArgoMuBai@outlook.com>
2021-11-17 18:00:16 +0800

    更新代码

---
+++ target/classes/datagrip/src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+export default new Vuex.Store({
+  state: {
+    user: {
+      username: window.localStorage.getItem('user' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('user' || '[]')).username
+    }
+  },
+  mutations: {
+    login (state, user) {
+      state.user = user
+      window.localStorage.setItem('user', JSON.stringify(user))
+    },
+    logout (state) {
+      var user = {
+        username: '',
+        password: ''
+      }
+      state.user = user
+      window.localStorage.setItem('user', JSON.stringify(user))
+    }
+  }
+})
